cae10f8fbbcbd2f73f54a5315ab160d3e3bf7d52
Rie Amasato <51439946+rie-amasato@users.noreply.github.com>
2025-05-28 16:34:17 +0000

    SSGのターゲット指定をしてみる

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,10 +1,11 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
-  compatibilityDate: '2025-05-15',
+  compatibilityDate: "2025-05-15",
   devtools: { enabled: true },
   css: ["amasugar/amasugar.css"],
+  ssr: true,
+  target: "static",
   app: {
-
-    buildAssetsDir: '/manekifeecalculator/',
-  }
-})
+    buildAssetsDir: "/manekifeecalculator/",
+  },
+});
